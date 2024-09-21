fa7fbefb05f9d24ba717e803c30e277b5ae15e7d
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-21 13:04:45 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+
+let currentAudio = null;
+
+
+function playSound(sound) {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0; 
+    }
+    
+    currentAudio = new Audio(sound);
+    currentAudio.play();
+}
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const sound = button.getAttribute('data-sound');
+        playSound(sound);
+    });
+});
+
+
+stopButton.addEventListener('click', () => {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0; // Reset time to 0
+    }
+});
